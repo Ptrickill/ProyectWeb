96fbf061993595118e7dd83b8f2308b8556022f9
Ptrickill <daniel.diaz.mosquera8@gmail.com>
2025-10-05 00:28:07 -0500

    Initial commit - Spring Boot project

---
+++ src/main/java/com/proyectoingweb/proyectoingweb/ProyectoingwebApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoingweb.proyectoingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoingwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoingwebApplication.class, args);
+	}
+
+}
